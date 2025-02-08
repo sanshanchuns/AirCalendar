1c306a7210777969ac165d85db4563cbb8c65903
zhuli <zhuli@kuaishou.com>
2025-02-08 11:22:50 +0800

    Initial Commit

---
+++ AirCalendar/AirCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirCalendarApp.swift
+//  AirCalendar
+//
+//  Created by leo on 2025/2/8.
+//
+
+import SwiftUI
+
+@main
+struct AirCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
